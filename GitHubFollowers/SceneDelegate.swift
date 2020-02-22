59d50f78a57b153e84f6b5532b02a84cfd07795b
James Shvarts <jshvarts@gmail.com>
2020-02-22 16:03:01 -0500

    Set up empty ViewController

--- GitHubFollowers/SceneDelegate.swift
+++ GitHubFollowers/SceneDelegate.swift
@@ -17,8 +17,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    guard let _ = (scene as? UIWindowScene) else { return }
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    // fill up entire screen
+    window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+    
+    // every window can has a window scene
+    window?.windowScene = windowScene
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
   }
+  
 
   func sceneDidDisconnect(_ scene: UIScene) {
     // Called as the scene is being released by the system.
