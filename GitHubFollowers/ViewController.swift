41a980f38c4afc0d6e8a2b926ba567a389f31592
James Shvarts <jshvarts@gmail.com>
2020-02-22 15:51:30 -0500

    New project, storyboard removed

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by James Shvarts on 2/22/20.
+//  Copyright © 2020 James Shvarts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
