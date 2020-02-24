6be1b5c62ce09ddaf00cdf56ea390a99304c31aa
James Shvarts <jshvarts@gmail.com>
2020-02-23 22:14:39 -0500

    Define and configure label

---
+++ GitHubFollowers/GFTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by James Shvarts on 2/23/20.
+//  Copyright © 2020 James Shvarts. All rights reserved.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  init(texAlignment: NSTextAlignment, fontSize: CGFloat) {
+    super.init(frame: .zero)
+    self.textAlignment = textAlignment
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+    configure()
+  }
+  
+  private func configure() {
+    textColor = .label
+    adjustsFontSizeToFitWidth = true
+    minimumScaleFactor = 0.9
+    lineBreakMode = .byTruncatingTail
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+}
