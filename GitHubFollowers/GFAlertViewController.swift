530d126672640a1ad6bccb042ccb6a07d76dd333
James Shvarts <jshvarts@gmail.com>
2020-02-24 08:50:42 -0500

    Configure GF alert view controller

--- GitHubFollowers/GFAlertViewController.swift
+++ GitHubFollowers/GFAlertViewController.swift
@@ -19,6 +19,8 @@ class GFAlertViewController: UIViewController {
   var message: String?
   var buttonTitle: String?
   
+  let padding: CGFloat = 20
+  
   init(title: String, message: String, buttonTitle: String) {
     super.init(nibName: nil, bundle: nil)
     self.alertTitle = title
@@ -34,6 +36,9 @@ class GFAlertViewController: UIViewController {
     super.viewDidLoad()
     view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
     configureContainerView()
+    configureTitleLabel()
+    configureActionButton()
+    configureMessageLabel()
   }
   
   private func configureContainerView() {
@@ -52,8 +57,45 @@ class GFAlertViewController: UIViewController {
     ])
   }
   
-  private func configureTitleView() {
+  private func configureTitleLabel() {
     containerView.addSubview(titleLabel)
     titleLabel.text = alertTitle ?? "Something went wrong"
+    
+    NSLayoutConstraint.activate([
+      titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: padding),
+      titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+      titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: padding),
+      titleLabel.heightAnchor.constraint(equalToConstant: 28)
+    ])
+  }
+  
+  private func configureActionButton() {
+    containerView.addSubview(actionButton)
+    actionButton.setTitle(buttonTitle ?? "Ok", for: .normal)
+    actionButton.addTarget(self, action: #selector(dismissViewController), for: .touchUpInside)
+    
+    NSLayoutConstraint.activate([
+      actionButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
+      actionButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+      actionButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+      actionButton.heightAnchor.constraint(equalToConstant: 44)
+    ])
+  }
+  
+  private func configureMessageLabel() {
+    containerView.addSubview(messageLabel)
+    messageLabel.text = message ?? "Unable to complete request"
+    messageLabel.numberOfLines = 4
+    
+    NSLayoutConstraint.activate([
+      messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+      messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+      messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+      messageLabel.bottomAnchor.constraint(equalTo: actionButton.topAnchor, constant: -12)
+    ])
+  }
+  
+  @objc private func dismissViewController() {
+    dismiss(animated: true, completion: nil)
   }
 }
