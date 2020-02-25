229cac248e13a377563b576581b089f9bd2ad5ff
James Shvarts <jshvarts@gmail.com>
2020-02-24 22:43:40 -0500

    Move showing navigation bar into viewWillAppear

--- GitHubFollowers/Screens/FollowersViewController.swift
+++ GitHubFollowers/Screens/FollowersViewController.swift
@@ -15,7 +15,11 @@ class FollowersViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
-    navigationController?.isNavigationBarHidden = false
     navigationController?.navigationBar.prefersLargeTitles = true
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.isNavigationBarHidden = false
   }
 }
