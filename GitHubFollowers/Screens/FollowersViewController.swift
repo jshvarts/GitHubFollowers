bb5ad74a610eb805260b3f175ee07f91cb42c5b7
James Shvarts <jshvarts@gmail.com>
2020-02-26 21:18:38 -0500

    Clean up followers view controller

--- GitHubFollowers/Screens/FollowersViewController.swift
+++ GitHubFollowers/Screens/FollowersViewController.swift
@@ -15,23 +15,19 @@ class FollowersViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureViewController()
     configureCollectionView()
-    view.backgroundColor = .systemBackground
-    navigationController?.navigationBar.prefersLargeTitles = true
-    
-    NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-      switch result {
-      case .success(let followers):
-        print("followers: \(followers)")
-      case .failure(let error):
-        self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
-      }
-    }
+    getFollowers()
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     navigationController?.setNavigationBarHidden(false, animated: true)
+  }
+  
+  private func configureViewController() {
+    view.backgroundColor = .systemBackground
+    navigationController?.navigationBar.prefersLargeTitles = true
   }
   
   private func configureCollectionView() {
@@ -40,5 +36,16 @@ class FollowersViewController: UIViewController {
     view.addSubview(collectionView)
     collectionView.backgroundColor = .systemPink
     collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
+  }
+  
+  private func getFollowers() {
+    NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+      switch result {
+      case .success(let followers):
+        print("followers: \(followers)")
+      case .failure(let error):
+        self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
+      }
+    }
   }
 }
