6e6527ede024ea80c616e6dd3b97da2de6880af8
James Shvarts <jshvarts@gmail.com>
2020-02-28 22:25:35 -0500

    use cache for avatar images

--- GitHubFollowers/CustomViews/GFAvatarImageView.swift
+++ GitHubFollowers/CustomViews/GFAvatarImageView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class GFAvatarImageView: UIImageView {
-  
+  let cache = NetworkManager.shared.cache
   let placeholderImage = UIImage(named: "avatar-placeholder")
   
   override init(frame: CGRect) {
@@ -29,6 +29,12 @@ class GFAvatarImageView: UIImageView {
   }
   
   func downloadImage(from urlString: String) {
+    let cacheKey = NSString(string: urlString)
+    if let image = cache.object(forKey: cacheKey) {
+      self.image = image
+      return
+    }
+    
     guard let url = URL(string: urlString) else { return }
     
     let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
@@ -38,6 +44,7 @@ class GFAvatarImageView: UIImageView {
       guard let data = data else { return }
       
       guard let image = UIImage(data: data) else { return }
+      self.cache.setObject(image, forKey: cacheKey)
       
       DispatchQueue.main.async {
         self.image = image
--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2020 James Shvarts. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
   static let shared = NetworkManager()
   let baseURL = "https://api.github.com/users/"
+  let cache = NSCache<NSString, UIImage>()
   
   private init() {}
   
