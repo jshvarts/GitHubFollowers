ba569c365b70cb757b0b3596a4ef6b6a79d782ba
James Shvarts <jshvarts@gmail.com>
2020-02-29 23:20:20 -0500

    set up item info custom view

--- GitHubFollowers/Utilities/Constants.swift
+++ GitHubFollowers/Utilities/Constants.swift
@@ -11,4 +11,8 @@ import Foundation
 // values found using the SF Symbols app
 enum SFSymbols {
   static let location = "mappin.and.ellipse"
+  static let repos = "folder"
+  static let gists = "text.alignleft"
+  static let followers = "heart"
+  static let following = "person.2"
 }
