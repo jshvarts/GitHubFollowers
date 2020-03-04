0927e6c9de1fdca50229869bdbbf0acecd9363e5
James Shvarts <jshvarts@gmail.com>
2020-03-03 22:14:27 -0500

    move download image function into network manager

--- GitHubFollowers/CustomViews/ViewControllers/GFUserInfoHeaderViewController.swift
+++ GitHubFollowers/CustomViews/ViewControllers/GFUserInfoHeaderViewController.swift
@@ -36,7 +36,7 @@ class GFUserInfoHeaderViewController: UIViewController {
   }
   
   private func configureUIElements() {
-    avatarImageView.downloadImage(from: user.avatarUrl)
+    downloadAvatarImage()
     usernameLabel.text = user.login
     nameLabel.text = user.name ?? ""
     locationLabel.text = user.location ?? "No Location"
@@ -45,6 +45,16 @@ class GFUserInfoHeaderViewController: UIViewController {
     
     locationImageView.image = UIImage(systemName: SFSymbols.location)
     locationImageView.tintColor = .secondaryLabel
+  }
+  
+  private func downloadAvatarImage() {
+    NetworkManager.shared.downloadImage(from: user.avatarUrl) { [weak self] image in
+      guard let self = self else { return }
+      
+      DispatchQueue.main.async {
+        self.avatarImageView.image = image
+      }
+    }
   }
   
   private func addSubviews() {
