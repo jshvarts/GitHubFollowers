816239353cfd4b63ace67d78cff311d3891e6954
James Shvarts <jshvarts@gmail.com>
2020-03-04 08:26:59 -0500

    remove unnecessary var in PersistenceManager

--- GitHubFollowers/Managers/PersistenceManager.swift
+++ GitHubFollowers/Managers/PersistenceManager.swift
@@ -22,21 +22,20 @@ enum PersistenceManager {
   static func updateWidth(favorite: Follower, actionType: PersistanceActionType, completed: @escaping (GFError?) -> Void) {
     retrieveFavorites { result in
       switch result {
-      case .success(let favorites):
-        var retrievedFavorites = favorites
+      case .success(var favorites):
         switch actionType {
         case .add:
-          guard !retrievedFavorites.contains(favorite) else {
+          guard !favorites.contains(favorite) else {
             return completed(.alreadyInFavorites)
           }
           
-          retrievedFavorites.append(favorite)
+          favorites.append(favorite)
           
         case .remove:
-          retrievedFavorites.removeAll { $0.login == favorite.login }
+          favorites.removeAll { $0.login == favorite.login }
         }
         
-        completed(save(favorites: retrievedFavorites))
+        completed(save(favorites: favorites))
         
       case .failure(let error):
         completed(error)
