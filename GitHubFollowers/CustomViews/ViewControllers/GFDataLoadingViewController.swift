df71f72d9bfc093f63d32136ef5b4e0528d986a6
James Shvarts <jshvarts@gmail.com>
2020-03-03 22:53:16 -0500

    constraint to containerView not view

--- GitHubFollowers/CustomViews/ViewControllers/GFDataLoadingViewController.swift
+++ GitHubFollowers/CustomViews/ViewControllers/GFDataLoadingViewController.swift
@@ -26,8 +26,8 @@ class GFDataLoadingViewController: UIViewController {
     
     activityIndicator.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint.activate([
-      activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-      activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+      activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+      activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)
     ])
     activityIndicator.startAnimating()
   }
