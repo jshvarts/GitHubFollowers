a42c9b6624463a918c3408834bf2613cae26e039
James Shvarts <jshvarts@gmail.com>
2020-03-04 07:36:57 -0500

    make full 3 lines of bio fit

--- GitHubFollowers/CustomViews/ViewControllers/GFUserInfoHeaderViewController.swift
+++ GitHubFollowers/CustomViews/ViewControllers/GFUserInfoHeaderViewController.swift
@@ -95,7 +95,7 @@ class GFUserInfoHeaderViewController: UIViewController {
       bioLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: textImagePadding),
       bioLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
       bioLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      bioLabel.heightAnchor.constraint(equalToConstant: 60)
+      bioLabel.heightAnchor.constraint(equalToConstant: 90)
     ])
   }
 }
--- GitHubFollowers/Screens/UserInfoViewController.swift
+++ GitHubFollowers/Screens/UserInfoViewController.swift
@@ -97,7 +97,7 @@ class UserInfoViewController: UIViewController {
     
     NSLayoutConstraint.activate([
       headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-      headerView.heightAnchor.constraint(equalToConstant: 180),
+      headerView.heightAnchor.constraint(equalToConstant: 210),
       
       itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: margin),
       itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
